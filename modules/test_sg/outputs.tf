5793b1e216c0007fb1307c6eaa4d357cc5085a83
jorgealziro <jorge.ardcs@gmail.com>
2021-03-15 01:10:06 -0300

    Created sg module

---
+++ modules/test_sg/outputs.tf
@@ -0,0 +1,3 @@
+output "public_id" {
+  value = aws_security_group.public_sg.id
+}
---
+++ modules/test_sg/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" {}
+variable "vpc_region" {}
+variable "sg_public" {}
+variable "subnet_public_cidr" {}
