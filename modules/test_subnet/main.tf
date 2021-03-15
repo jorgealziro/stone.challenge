163a8d7a296cd32dae7c8ee6b7f876fc4a429514
jorgealziro <jorge.ardcs@gmail.com>
2021-03-14 23:41:23 -0300

    Created subnet module

---
+++ modules/test_subnet/main.tf
@@ -0,0 +1,9 @@
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = var.vpc_id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.subnet_az
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
