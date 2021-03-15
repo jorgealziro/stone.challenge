cc8aee4e1aa0de59dc51299a2a2241d099334c6f
jorgealziro <jorge.ardcs@gmail.com>
2021-03-14 23:42:07 -0300

    Implemented public subnet to root terraform file

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 module "test_vpc" {
   source = "./modules/test_vpc"
-   
+
   vpc_region =  var.vpc_region
   vpc_name =  var.vpc_name
   vpc_cidr_block =  var.vpc_cidr_block
 
 }
 
+module "subnet_public" {
+  source = "./modules/test_subnet"
 
+  vpc_id      = module.test_vpc.id
+  vpc_region  = module.test_vpc.region
+  subnet_name = var.subnet_name
+  subnet_cidr = var.subnet_public_cidr
+  subnet_az   = var.subnet_public_az
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,20 @@ variable "vpc_name" {
 variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
+
+# Subnet Config
+
+variable "subnet_name" {
+  description = "Public subnet"
+  default     = "Public Test Subnet"
+}
+
+variable "subnet_public_cidr" {
+  description = "CIDR for public subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_public_az" {
+  description = "Availability zone for public subnet"
+  default     = "us-west-2a"
+}
